81218cd3234c624cd1e5eabd1dd2650741148133
Gracmit <aleksii97@gmail.com>
2023-12-19 13:57:49 +0200

    Added first kitchen counter to the game and raycast to detect them

--- Unity Online/Assets/Scripts/Player.cs
+++ Unity Online/Assets/Scripts/Player.cs
@@ -1,13 +1,28 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 7f;
     [SerializeField] private GameInput _gameInput;
+    [SerializeField] private LayerMask _countersLayerMask;
 
     private bool _isWalking;
+    private Vector3 lastInteractionDirection;
     
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+
+    public bool IsWalking()
+    {
+        return _isWalking;
+    }
+
+    public void HandleMovement()
     {
         var inputVector = _gameInput.GetMovementVectorNormalized();
         var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
@@ -35,9 +50,7 @@ public class Player : MonoBehaviour
                     moveDirection = moveDirectionZ;
             }
         }
-        
-        
-        
+
         if(canMove)
             transform.position += moveDirection * (moveDistance);
 
@@ -46,9 +59,22 @@ public class Player : MonoBehaviour
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
     }
-
-    public bool IsWalking()
+    
+    private void HandleInteractions()
     {
-        return _isWalking;
+        var inputVector = _gameInput.GetMovementVectorNormalized();
+        var moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDirection != Vector3.zero)
+            lastInteractionDirection = moveDirection;
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractionDirection, out RaycastHit raycastHit, interactDistance, _countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
     }
 }
