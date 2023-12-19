6427dd0fc731951ce31485eece9a66716de7a6f5
Gracmit <aleksii97@gmail.com>
2023-12-19 13:23:04 +0200

    Custom collision detection

--- Unity Online/Assets/Scripts/Player.cs
+++ Unity Online/Assets/Scripts/Player.cs
@@ -11,7 +11,35 @@ public class Player : MonoBehaviour
     {
         var inputVector = _gameInput.GetMovementVectorNormalized();
         var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDirection * (_moveSpeed * Time.deltaTime);
+
+        float moveDistance = _moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove)
+            {
+                moveDirection = moveDirectionX;
+            }
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if (canMove)
+                    moveDirection = moveDirectionZ;
+            }
+        }
+        
+        
+        
+        if(canMove)
+            transform.position += moveDirection * (moveDistance);
 
         _isWalking = moveDirection != Vector3.zero;
 
