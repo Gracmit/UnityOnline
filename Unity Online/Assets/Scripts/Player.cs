d29537e9fb3d21b4e709fb2e9a3f2b0ed0c01081
Gracmit <aleksii97@gmail.com>
2023-12-19 12:58:11 +0200

    Player input refactoring to use the new input system

--- Unity Online/Assets/Scripts/Player.cs
+++ Unity Online/Assets/Scripts/Player.cs
@@ -3,26 +3,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 7f;
+    [SerializeField] private GameInput _gameInput;
 
     private bool _isWalking;
     
     private void Update()
     {
-        Vector2 inputVector = Vector2.zero;
-        if(Input.GetKey(KeyCode.W))
-            inputVector.y =+ 1;
-        
-        if(Input.GetKey(KeyCode.S))
-            inputVector.y =- 1;
-        
-        if(Input.GetKey(KeyCode.A))
-            inputVector.x =- 1;
-        
-        if(Input.GetKey(KeyCode.D))
-            inputVector.x =+ 1;
-
-        inputVector = inputVector.normalized;
-
+        var inputVector = _gameInput.GetMovementVectorNormalized();
         var moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * (_moveSpeed * Time.deltaTime);
 
